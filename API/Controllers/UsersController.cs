b97bdb0ddeeeeb848afd7f551ff5dd93a0a92e0f
s0gemaker <s0gemaker@gmail.com>
2021-05-01 00:48:25 -0400

    jwt token user authentication done

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,14 +3,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -19,6 +19,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
 
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -26,11 +27,12 @@ namespace API.Controllers
 
         }
 
+        [Authorize]
         [HttpGet("{id}")]
 
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            return await _context.Users.FindAsync(id);           
+            return await _context.Users.FindAsync(id);
         }
 
 
